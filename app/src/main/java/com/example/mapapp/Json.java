08b305d531e17904ff785d4b6efc57a28a5ceee3
Caleb Smith <CRSmith4@student.fullsail.edu>
2019-11-05 13:36:57 -0500

    Added directions between two points

---
+++ app/src/main/java/com/example/mapapp/Json.java
@@ -0,0 +1,14 @@
+package com.example.mapapp;
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Json {
+
+}
